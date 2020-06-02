11508d47e178aa6551e72174b17a55c90f120bd5
iurii <enclaveunit@gmail.com>
2020-06-02 19:44:04 -0400

    faculties page start

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,321 @@
-!function(u){u(function(){function e(){var e=u(".excellence__list");(u(window).outerWidth()<1025?e.addClass("excellence__slider"):e.removeClass("excellence__slider"),u(".excellence__slider").length)&&u(".excellence__slider").slick({vertical:!1,dots:!1,infinite:!0,arrows:!1,nextArrow:"",prevArrow:"",slidesToScroll:1,variableWidth:!0,centerMode:!0})}var i,o,s;function n(){var e=u(".price__list");(u(window).outerWidth()<1025?e.addClass("price__slider"):e.removeClass("price__slider"),u(".price__slider").length)&&u(".price__slider").slick({vertical:!1,dots:!0,infinite:!0,arrows:!1,nextArrow:"",prevArrow:"",slidesToScroll:1,variableWidth:!0,centerMode:!0})}function l(){var e=u(".services__list");(u(window).outerWidth()<768?e.addClass("services__slider"):e.removeClass("services__slider"),u(".services__slider").length)&&u(".services__slider").slick({vertical:!1,dots:!0,infinite:!0,arrows:!1,nextArrow:"",prevArrow:"",slidesToScroll:1,variableWidth:!0,centerMode:!0})}function t(){u(document).on("scroll",r)}function r(){var e=u(".up__btn");500<u(document).scrollTop()?e.not(".visible").addClass("visible"):e.removeClass("visible")}(new WOW).init(),u(".js_nav").on("click",function(e){e.preventDefault();var i=u("body");i.hasClass("mobile-menu--open")?i.removeClass("mobile-menu--open"):i.addClass("mobile-menu--open")}),u(".js_select").selectize({create:!1,maxItems:1,valueField:"value",labelField:"title",searchField:["title"]}),e(),u(window).on("resize",function(){e()}),i=u(".slider__news"),o=u(".slider-news__nav--right"),s=u(".slider-news__nav--left"),i.slick({vertical:!1,dots:!0,infinite:!0,arrows:!0,speed:500,cssEase:"linear",nextArrow:"",prevArrow:"",slidesToShow:3,slidesToScroll:1,variableWidth:!1,centerMode:!1,responsive:[{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1,infinite:!0,dots:!0}},{breakpoint:425,settings:{slidesToShow:1,slidesToScroll:1,infinite:!0,dots:!0}}]}),o.on("click",function(e){e.preventDefault(),i.slick("slickNext")}),s.on("click",function(e){e.preventDefault(),i.slick("slickPrev")}),n(),u(window).on("resize",function(){n()}),l(),u(window).on("resize",function(){l()}),u(".form-submit").on("click",function(){var e=u(this).closest(".form"),i=e.find('input[name="name"]').val(),o=e.find('input[name="phone"]').val(),s=e.find('input[name="email"]').val();return u.ajax({type:"POST",url:"http://35it.ru/mail.php",data:{name:i,phone:o,email:s},success:function(e){"1"===e&&(console.log(e),console.log("Ok!"),u(".success").show())}}),!1}),t(),u(".js-scroll-arrow").on("click.smoothscroll",function(e){e.preventDefault();var i,o=this.hash,s=o.replace(/#/,"");i=""==s?u("body"):u(o+', a[name="'+s+'"]').first();var n=u(this).attr("href").replace(/#.*/,"");!i.length||n&&location.href.replace(/#.*/,"")!=n||u("html, body").stop().animate({scrollTop:i.offset().top},1e3,"swing",function(){window.location.hash=o})}),u(document).on("scroll",a);var c=0;function a(){var e=u(".js_fixedMenu"),i=u(window).height()-50,o=u(document).scrollTop();o<i&&o<c?e.removeClass("fixed-menu--show"):i<o&&e.not(".fixed-menu--show").addClass("fixed-menu--show"),c=o}function d(e){e.not(".hidden").addClass("hidden"),e.find(".js_modalWindowBack").removeAttr("style"),u("html").removeAttr("style")}function t(){u(document).on("scroll",r)}function r(){var e=u(".up__btn");500<u(document).scrollTop()?e.not(".visible").addClass("visible"):e.removeClass("visible")}a(),u(".js_modal").on("click touchend",function(e){e.preventDefault();var i=u(e.currentTarget).data("target");u("#"+i).removeClass("hidden"),u("html").css({overflow:"hidden"})}),u(".js_modalWindowBack").on("click",function(e){0<u(e.target).closest(".js_modalWindowContent").length||(d(u(e.currentTarget).closest(".js_modalWindow")),e.stopPropagation())}),u(".js_close-modal-window").on("click touchend",function(e){d(u(e.currentTarget).closest(".js_modalWindow")),e.stopPropagation()}),t()})}(jQuery);
+(function ($) {
+    $(function() {
+        // 1. Animate ---------------------------
+        new WOW().init();
+
+        // 2. Open main menu mobile version ---------------------------
+        function openMainMenu() {
+            $('.js_nav').on('click', function (e) {
+                e.preventDefault();
+                var body = $('body');
+                if (body.hasClass('mobile-menu--open')) {
+                    body.removeClass('mobile-menu--open');
+                } else {
+                    body.addClass('mobile-menu--open');
+                }
+            });
+        }
+
+        openMainMenu();
+
+        // 3. Selects ---------------------------
+        function customSelect() {
+            $('.js_select').selectize({
+                create: false,
+                //sortField: 'text'
+                maxItems: 1,
+                valueField: 'value',
+                labelField: 'title',
+                searchField: ['title']
+            });
+        }
+
+        customSelect();
+
+
+        // 4. Mobile slider excellence ---------------------------
+        function sliderExcellenceMobile() {
+            var excellenceList = $('.excellence__list'),
+                winW = $(window).outerWidth();
+
+            if(winW < 1025) {
+                excellenceList.addClass('excellence__slider');
+            }
+            else {
+                excellenceList.removeClass('excellence__slider');
+            }
+
+            if($('.excellence__slider').length) {
+                var mobileSlider = $('.excellence__slider');
+
+                mobileSlider.slick({
+                    vertical: false,
+                    dots: false,
+                    infinite: true,
+                    arrows: false,
+                    nextArrow: '',
+                    prevArrow: '',
+                    slidesToScroll: 1,
+                    variableWidth: true,
+                    centerMode: true
+                });
+            }
+        }
+
+        sliderExcellenceMobile();
+        $(window).on('resize', function(){
+            sliderExcellenceMobile();
+        });
+
+
+        // 6. Slider news ---------------------------
+        function sliderNews() {
+            var sliderNews = $('.slider__news'),
+                next = $('.slider-news__nav--right'),
+                prev = $('.slider-news__nav--left');
+
+            sliderNews.slick({
+                vertical: false,
+                dots: true,
+                infinite: true,
+                arrows: true,
+                speed: 500,
+                cssEase: 'linear',
+                nextArrow: '',
+                prevArrow: '',
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                variableWidth: false,
+                centerMode: false,
+                responsive: [
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 425,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: true
+                    }
+                }],
+            });
+
+            next.on('click', function(e) {
+                e.preventDefault();
+                sliderNews.slick('slickNext');
+            });
+
+            prev.on('click', function(e) {
+                e.preventDefault();
+                sliderNews.slick('slickPrev');
+            });
+        }
+
+        sliderNews();
+
+        // 7. Mobile slider price ---------------------------
+        function sliderPriceMobile() {
+            var priceList = $('.price__list'),
+                winW = $(window).outerWidth();
+
+            if(winW < 1025) {
+                priceList.addClass('price__slider');
+            }
+            else {
+                priceList.removeClass('price__slider');
+            }
+
+            if($('.price__slider').length) {
+                var mobileSlider = $('.price__slider');
+
+                mobileSlider.slick({
+                    vertical: false,
+                    dots: true,
+                    infinite: true,
+                    arrows: false,
+                    nextArrow: '',
+                    prevArrow: '',
+                    slidesToScroll: 1,
+                    variableWidth: true,
+                    centerMode: true
+                });
+            }
+        }
+
+        sliderPriceMobile();
+        $(window).on('resize', function(){
+            sliderPriceMobile();
+        });
+
+        // 8. Mobile slider services ---------------------------
+        function sliderServicesMobile() {
+            var servicesList = $('.services__list'),
+                winW = $(window).outerWidth();
+
+            if(winW < 768) {
+                servicesList.addClass('services__slider');
+            }
+            else {
+                servicesList.removeClass('services__slider');
+            }
+
+            if($('.services__slider').length) {
+                var mobileSlider = $('.services__slider');
+
+                mobileSlider.slick({
+                    vertical: false,
+                    dots: true,
+                    infinite: true,
+                    arrows: false,
+                    nextArrow: '',
+                    prevArrow: '',
+                    slidesToScroll: 1,
+                    variableWidth: true,
+                    centerMode: true
+                });
+            }
+        }
+
+        sliderServicesMobile();
+        $(window).on('resize', function(){
+            sliderServicesMobile();
+        });
+
+
+		// 9. Fix share on scroll page
+        $('.form-submit').on('click', function(){
+          var formItem = $(this).closest('.form');
+
+          var name = formItem.find('input[name="name"]').val();
+          var phone = formItem.find('input[name="phone"]').val();
+          var email = formItem.find('input[name="email"]').val();
+
+          $.ajax(
+              {
+                  type: "POST",
+                  url: 'http://35it.ru/mail.php',
+                  data: { name: name, phone: phone, email: email },
+                  success: function (data) {
+                      if(data === '1') {
+                          console.log(data);
+                          console.log("Ok!");
+                          $('.success').show();
+                      }
+                  }
+              }
+          );
+          return false;
+        });
+
+        // 10. Fix share on scroll page
+        function bindEventHandlers() {
+            $(document).on("scroll", eventTopButton);
+        }
+
+        function eventTopButton() {
+            var upBtn = $(".up__btn");
+
+            ($(document).scrollTop() > 500) ? upBtn.not(".visible").addClass("visible") : upBtn.removeClass("visible");
+        }
+
+        bindEventHandlers();
+
+        // 11. Smooth Scroll Link
+        function addSmoothScroll() {
+            $('.js-scroll-arrow').on('click.smoothscroll', function(e){
+                e.preventDefault();
+
+                var hash = this.hash, $target, hashClear = hash.replace(/#/,'');
+                if( hashClear == '' ) $target = $('body');
+                else $target = $( hash + ', a[name="'+ hashClear +'"]').first();
+                var theHref = $(this).attr('href').replace(/#.*/,'');
+                if( ! $target.length || (theHref && location.href.replace(/#.*/,'') != theHref)  ) return;
+
+                $('html, body').stop().animate({ scrollTop: $target.offset().top }, 1000, 'swing', function(){
+                    window.location.hash = hash;
+                });
+            });
+        }
+
+        addSmoothScroll();
+
+
+        /*
+         * 12. Show fixed menu when scrolling window
+         * */
+        $(document).on('scroll', documentOnScrollEventHandler);
+        var prevScrollPosition = 0;
+
+        function documentOnScrollEventHandler() {
+            var fixedMenu = $('.js_fixedMenu'),
+                point = $(window).height() - 50,
+                currentScrollPosition = $(document).scrollTop();
+
+            if (currentScrollPosition < point && prevScrollPosition > currentScrollPosition) {
+                fixedMenu.removeClass('fixed-menu--show');
+            } else if(currentScrollPosition > point) {
+                fixedMenu.not('.fixed-menu--show').addClass('fixed-menu--show');
+            }
+            prevScrollPosition = currentScrollPosition;
+        }
+
+        documentOnScrollEventHandler();
+
+        /*
+         * modal window
+         * */
+        $('.js_modal').on('click touchend', function(e){
+            e.preventDefault();
+            var $el = $(e.currentTarget);
+            var targetId = $el.data('target');
+            var target = $('#' + targetId);
+            target.removeClass('hidden');
+            $('html').css({
+                overflow: 'hidden'
+            });
+        });
+
+        $('.js_modalWindowBack').on('click', function(e) {
+            if ($(e.target).closest('.js_modalWindowContent').length > 0) {
+                return;
+            }
+            var $el = $(e.currentTarget);
+            var parent = $el.closest('.js_modalWindow');
+            closeModal(parent);
+            e.stopPropagation();
+        });
+
+        $('.js_close-modal-window').on('click touchend', function(e) {
+            var parent = $(e.currentTarget).closest('.js_modalWindow');
+            closeModal(parent);
+            e.stopPropagation();
+        });
+
+        function closeModal(modal){
+            modal.not('.hidden').addClass('hidden');
+            modal.find('.js_modalWindowBack').removeAttr('style');
+            $('html').removeAttr('style');
+        }
+
+        // 11. Scroll down init
+        function bindEventHandlers() {
+            $(document).on("scroll", eventTopButton);
+        }
+
+        function eventTopButton() {
+            var upBtn = $(".up__btn");
+
+            ($(document).scrollTop() > 500) ? upBtn.not(".visible").addClass("visible") : upBtn.removeClass("visible");
+        }
+
+        bindEventHandlers();
+    });
+})(jQuery);
